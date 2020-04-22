79fa2855ccd1738f9844b3d225bf2416a30a74c6
Helle <Fjelding>
2020-04-22 13:44:47 +0200

    skrift mm

--- pages/homePage.js
+++ pages/homePage.js
@@ -8,11 +8,11 @@ export default class HomePage {
             <article id="forside" class="page">
             <h1>Arlagården Plus</h1>
             <div class="flexContainer">
-                <div class="frontPageBoks1" id="boks1"><img src="../img/data.svg"><h2>Indtast Data</h2></div>
-                <div class="frontPageBoks1" id="boks2"><img src="../img/graf.svg"><h2>Data og Medaljer</h2></div>
+                <div class="frontPageBoks1" id="boks1"><img src="../img/data.svg"><h2 class="h2Home">Indtast Data</h2></div>
+                <div class="frontPageBoks1" id="boks2"><img src="../img/graf.svg"><h2 class="h2Home">Data og Medaljer</h2></div>
                 <div class="frontPageBoks1"><div class="frontPageCircle"><img src="../img/Arla_Logo_RGB.svg"> </div></div>
-                <div class="frontPageBoks1" id="boks3"><img src="../img/danmark.svg"><h2>Sammenligning</h2></div>
-                <div class="frontPageBoks1" id="boks4"><img src="../img/tiltag.svg"><h2>Mulige Tiltag</h2></div>
+                <div class="frontPageBoks1" id="boks3"><img src="../img/danmark.svg"><h2 class="h2Home">Sammenligning</h2></div>
+                <div class="frontPageBoks1" id="boks4"><img src="../img/tiltag.svg"><h2 class="h2Home">Mulige Tiltag</h2></div>
             </div>
             <div id="frontPageTekst">
             <h3>Her har du mulighed for:</h3>
