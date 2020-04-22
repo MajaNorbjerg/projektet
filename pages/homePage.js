697f3c79d1b92b121d579f92d399b1041e2db3b4
Helle <Fjelding>
2020-04-22 09:42:59 +0200

    rettelser forside

--- pages/homePage.js
+++ pages/homePage.js
@@ -8,11 +8,11 @@ export default class HomePage {
             <article id="forside" class="page">
             <h1>Arlagården Plus</h1>
             <div class="flexContainer">
-                <div class="frontPageBoks1" id="boks1"><img src="../img/indtastData.svg"></div>
-                <div class="frontPageBoks2" id="boks2"><img src="../img/graf.svg"></div>
+                <div class="frontPageBoks1" id="boks1"><img src="../img/data.svg"><h2>Indtast Data</h2></div>
+                <div class="frontPageBoks2" id="boks2"><img src="../img/graf.svg"><h2>Data og Medaljer</h2></div>
                 <div class="frontPageCircle"><img src="../img/Arla_Logo_RGB.svg"> </div>
-                <div class="frontPageBoks2" id="boks3"><img src="../img/danmark.svg"></div>
-                <div class="frontPageBoks1" id="boks4"><img src="../img/lightballFull.svg"></div>
+                <div class="frontPageBoks2" id="boks3"><img src="../img/danmark.svg"><h2>Sammenligning</h2></div>
+                <div class="frontPageBoks1" id="boks4"><img src="../img/tiltag.svg"><h2>Mulige Tiltag</h2></div>
                 </div>
             </article>`
     }
