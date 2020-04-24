c7d5027092480dab634443af711a09b26b218706
Maja Jensen <eaamnje@students.eaaa.dk>
2020-04-24 12:16:49 +0200

    chart

--- services/chartAdd.js
+++ services/chartAdd.js
@@ -6,58 +6,31 @@ class ChartAdd {
     constructor() {
         // this.template();
         this.chart;
+        // this.startChart();
     }
 
-    // async init() {
-    //     // user 1 - auth user 
-    //     // let = authService.authUser.uid;
-    //     let uid = "j7WsepsaogO7mvb2S35LEfdQLmq1"; // using a fixed uid - want to make sure there's data matching an uid in the database
-    //     let data = await sustainabilityDataService.getPreparedDataByUid(uid); // getting prepared data from the service
-    //     let chartContainer = document.getElementById('chartContainer');
-    //     this.chart = new Chart(chartContainer, {
-    //         type: 'line',
-    //         data: {
-    //             datasets: [
-    //                 // first iniial dataset - auth user
-    //                 {
-    //                     data: data.milkProduction,
-    //                     label: 'User: AuthUser',
-    //                     fill: false,
-    //                     borderColor: "#e755ba",
-    //                     backgroundColor: "#e755ba",
-    //                     pointBackgroundColor: "#55bae7",
-    //                     pointBorderColor: "#55bae7",
-    //                     pointHoverBackgroundColor: "#55bae7",
-    //                     pointHoverBorderColor: "#55bae7",
-    //                 }
-    //             ],
-    //             labels: data.years
-    //         }
-    //     });
-    // }
+    // dataInput(data){
 
-    // template() {
-    //     document.getElementById('content').innerHTML += /*html*/ `
-    //   <section id="chartCompare" class="page">
-    //     <header class="topbar">
-    //       <h2>Charts Add Dataset</h2>
-    //     </header>
-    //     <h3>Milk Production pr cow</h3>
-    //     <canvas id="milkProductionCompare"></canvas>
-    //     <button onClick="addDataset()">Add Dataset</button>
-    //   </section>
-    // `;
+    startChart() {
+        let checkBox = document.querySelector('#northDenmark');
+        if (checkBox.checked) {
+            this.addDataset(checkBox, '7OIHxbSLJcSF2sXVtxTA', 'dieselMyData', '#147896')
+        }
+
+    }
+
     // }
 
     async addDataset(element, id, data, color) {
         console.log(element);
         console.log(element.checked);
-
+        console.log(data)
 
 
         if (element.checked) {
             // user 2 - Ep7o7EToQtZzdKnEDy2ahirFHc43
             let uidCompare = id; // matching an uid from the database
+            console.log(uidCompare)
             let dataCompare = await sustainabilityDataService.getPreparedDataByUid(uidCompare); // getting prepared data from the service
             console.log(dataCompare)
             // creating the dataset to add
@@ -94,11 +67,7 @@ class ChartAdd {
 
 
         chartService.chart.options = {
-            responsive: true,
-            title: {
-                display: true,
-                text: 'Chart.js'
-            },
+
             scales: {
                 xAxes: [{
                     ticks: {
@@ -113,47 +82,6 @@ class ChartAdd {
         };
         chartService.chart.update();
 
-
-        // console.log(element.id)
-        // if (element.id === 'fromYear') {
-
-        //     if (element.value === 2017) {
-        //         chartService.chart.data.labels.slice(1, 3);
-        //         chartService.chart.data.datasets.forEach((dataset) => {
-        //             console.log(dataset.data)
-        //         });
-
-        //     }
-
-        //     chartService.chart.data.labels.shift();
-
-        //     // let spliced = arr.slice(0, 3)
-        //     // console.log(arr, spliced)
-        //     chartService.chart.data.datasets.forEach((dataset) => {
-        //         console.log(dataset.data)
-        //         dataset.data.shift();
-
-        //     });
-
-        // } else if (element.id === 'toYear') {
-        //     console.log(element.id)
-
-
-        //     chartService.chart.data.labels.pop();
-        //     chartService.chart.data.labels.pop();
-        //     // let spliced = arr.slice(0, 3)
-        //     // console.log(arr, spliced)
-        //     chartService.chart.data.datasets.forEach((dataset) => {
-        //         console.log(dataset.data)
-
-        //         dataset.data.pop();
-        //         dataset.data.pop();
-        //     });
-        // }
-        // // fromYear
-        // // toYear
-
-        // chartService.chart.update();
     }
 
     mapToChart(element, checkboxId, id, color) {
@@ -171,6 +99,7 @@ class ChartAdd {
             element.style.stroke = "none"
         }
     }
+
 
 
 
