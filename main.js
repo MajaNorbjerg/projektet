b9be4f1378d3acb4309379265d5aa24ea54a760e
Johanne Justesen <johannejpoulsen@gmail.com>
2020-04-24 08:44:49 +0200

    rettelser

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ window.border = (x) => compareDataPage.border(x);
 window.ikkeBorder = (x) => compareDataPage.ikkeBorder(x);
 window.farveskift1 = () => compareDataPage.farveskift1();
 window.showFlower = () => compareDataPage.showFlower();
-window.clickBorder = () => compareDataPage.clickBorder();
 
 window.createFarmer = () => dataService.createFarmer();
 // window.appendChart = () => chartService.appendChart();
